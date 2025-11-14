09630a348b63dd5235055f8842f8a1bb59183106
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-11-14 21:45:20 +0100

    Create ImgWrapper.vue

---
+++ src/components/ImgWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+    <div 
+        class="relative w-full h-[500px] md:h-[60vw] max-h-[700px] rounded-3xl overflow-hidden"
+        style="background: linear-gradient(to bottom, #9dc1e6, #ecdfd1);"
+    >
+        <img
+            :src="src"
+            class="absolute inset-0 m-auto max-w-[90%] max-h-[90%] object-contain rounded-2xl"
+        />
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  src: string;
+}>();
+</script>
