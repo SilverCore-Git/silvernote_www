8b03857dd825392ef52c10df159ac9a24b957c6d
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-11-14 18:55:25 +0100

    add image annimation on hero

--- src/views/Home/Hero.vue
+++ src/views/Home/Hero.vue
@@ -1,25 +1,66 @@
 <script setup lang="ts">
-import SButton from '../../components/SButton.vue';
+import { onMounted, nextTick } from "vue";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SButton from "../../components/SButton.vue";
 
+gsap.registerPlugin(ScrollTrigger);
+
+onMounted(async () => {
+
+    await nextTick();
+
+    // État initial
+    gsap.set(".hero-img", {
+        rotateX: 40,
+        y: -80,
+        opacity: 1,
+        scale: 0.8,
+        transformOrigin: "center center",
+    });
+
+    // Animation
+    gsap.to(".hero-img", {
+        rotateX: 0,
+        y: 0,
+        opacity: 1,
+        scale: 1,
+        ease: "none",
+
+        scrollTrigger: {
+            trigger: ".hero-img",
+            start: "top 80%",
+            end: "top 20%",
+            scrub: 1.5,
+            markers: false, // mets true pour debug
+        },
+    });
+
+    setTimeout(() => {
+        ScrollTrigger.refresh();
+    }, 200);
+
+});
 </script>
+
 
 <template>
 
     <section 
-        class="flex justify-center items-center flex-col pt-[13%] bg"
+        class="flex justify-center items-center flex-col pt-[13%] bg shadow"
     >
         <div 
             class="max-w-5xl text-center flex justify-center items-center flex-col gap-6"
         >
 
-            <div class="text-[76px] font-bold max-w-4xl ">
+            <h1 class="max-w-4xl">
                 Vos notes <span class="text-(--primary)">accessibles</span> <span class="text-(--primary)">partout</span> et simplement
-            </div>
+            </h1>
 
-            <div class="mb-4 text-[20px]">
+            <p class="mb-4 text-[20px]">
                 SilverNote est l'application de prise de notes simple et intuitive conçue pour vous,
                 <br />disponible sur mobile, PC et en tant qu'application web.
-            </div>
+            </p>
 
             <div class="flex justify-center items-center my-8 gap-4">
                 <SButton
@@ -31,13 +72,17 @@ import SButton from '../../components/SButton.vue';
                 />
             </div>
 
-            <img 
-                class="rounded-2xl"
-                src="/assets/img/silvernote_desktop_screen.png"
-            />
+            <div class=" perspective-[2000px]">
+                <img 
+                    class="rounded-2xl hero-img "
+                    src="/assets/img/silvernote_desktop_screen.png"
+                />
+            </div>
 
         </div>
+
     </section>
+
 </template>
 
 <style scoped>
