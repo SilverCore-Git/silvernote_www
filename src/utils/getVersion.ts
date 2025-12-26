4ded8837dce36c872319ab675dd8f9d79ec12dc4
mister_papaye <philippeetchtebaise@hotmail.com>
2025-12-26 11:37:20 +0100

    Create getVersion.ts

---
+++ src/utils/getVersion.ts
@@ -0,0 +1,22 @@
+export async function getVersionFromGitHub
+(repo: string, branch: string = "main")
+{
+    const url = `https://raw.githubusercontent.com/${repo}/${branch}/package.json`;
+
+    try {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Erreur HTTP ${res.status}`);
+        }
+
+        const pkg = await res.json();
+        return pkg.version;
+    } catch (err) {
+        console.error("Impossible de récupérer la version :", err);
+        return null;
+    }
+}
+
+
+export default getVersionFromGitHub("SilverCore-Git/silvernote_app", "main");
