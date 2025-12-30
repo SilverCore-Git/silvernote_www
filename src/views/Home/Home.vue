3d4a0279ce120573694b6a562cd1a1cc55faae0c
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-12-30 14:53:15 +0100

    a commit

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,22 +1,20 @@
 <template>
     
-    <SmothScrollWrapper>
-        <div class="space-y-40">
-            <Hero />
-            <SyncWork />
-            <Features />
-            <!-- <TestimonialsSection /> -->
-            <!-- <PricingSection /> -->
-            <FAQSection />
-            <UseSnote />
-        </div>
-    </SmothScrollWrapper>
+    <div class="space-y-40">
+        <Hero />
+        <SyncWork />
+        <Features />
+        <!-- <TestimonialsSection /> -->
+        <!-- <PricingSection /> -->
+        <FAQSection />
+        <UseSnote />
+    </div>
 
 </template>
 
 <script lang="ts" setup>
 import Hero from './Hero.vue';
 import SyncWork from './syncWork.vue';
-import { SmothScrollWrapper, FAQSection, UseSnote } from '@/components';
+import { FAQSection, UseSnote } from '@/components';
 import Features from './Features.vue';
 </script>
